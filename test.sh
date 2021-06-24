34c1e2fb1641cf5f8043d7664f73452cace4f475
samir Menaa <davidolivares68@gmail.com>
2021-06-24 17:45:48 +0200

    added db file containing our custom motion reference

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/dog_pace.txt --model_file ../model_saves/model_+280M_tweaked_reward.zip --visualize
+python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/custom_ref_1.txt --model_file ../model_saves/model_+280M_tweaked_reward.zip --visualize
