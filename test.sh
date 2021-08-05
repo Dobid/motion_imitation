d1f3f3c49d71a62648385dd79b61651e4c0402d8
samir Menaa <davidolivares68@gmail.com>
2021-08-05 16:52:20 +0200

    new end eff sigmoid based reward function

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_train_18.txt --model_file model_saves/static_turn_quater_data_18/model_138M.zip --visualize --sync_reference
+# python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/turn.txt --model_file model_saves/base_turn_quater_endeff_rew/model8.057.zip --visualize --sync_reference
+python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_train_18.txt --model_file model_saves/stq_d18_endeff_rew_2/model21M_16.27.zip --visualize --sync_reference
 # python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_yaw_test.txt --model_file model_saves/cmd_noise.zip --visualize
