5ad4302ef96cd2449ae2d39312496632e3224cc8
samir Menaa <davidolivares68@gmail.com>
2021-08-09 14:30:06 +0200

    few corrections

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/turn.txt --model_file model_saves/base_turn_quater_endeff_rew/model8.057.zip --visualize --sync_reference
-python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_train_19.txt --model_file model_saves/stq_d18_sigrew/model110.48_30M.zip --visualize
+python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_train_19.txt --model_file model_saves/stq_d19_sigrew/model46.34.zip --visualize
 # python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/data_yaw_test.txt --model_file model_saves/cmd_noise.zip --visualize
